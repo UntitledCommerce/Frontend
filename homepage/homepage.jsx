7af92d55dddf2308d534f59a00868eae7be7a4f1
dani <xdakatabg@gmail.com>
2025-02-12 22:55:00 +0200

    homepage jsx reworked

--- homepage/homepage.jsx
+++ homepage/homepage.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
+import Navbar from '../components/navbar/navbar';
 function App() {
     return (
       <div className="App">
         {/* Navigation Bar */}
-        <nav className="navbar">
-          <div className="logo">TechZone</div>
-          <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#products">Products</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-  
+        <Navbar /> 
         {/* Hero Section */}
         <section id="home" className="hero">
           <div className="hero-content">
