3762833603a0b75bbc43b4aac3df104089d60f22
Slavi Panayotov <slavipanaiotov3@gmail.com>
2025-02-18 15:19:22 +0200

    Added css to the about \page

---
+++ AboutPage/AboutPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Navbar from '../components/navbar/navbar';
+function App() {
+    return (
+      <div className="App">
+        {/* Navigation Bar */}
+        <Navbar /> 
+        </div>)
+}
